162cd06d81905ab151b8da48dc19239cdb6c961a
Fabian Schmid <fabian@sr.solutions>
2022-02-09 12:07:19 +0100

    emoved old sendXY methods

--- Services/Utilities/classes/class.ilUtil.php
+++ Services/Utilities/classes/class.ilUtil.php
@@ -1156,72 +1156,7 @@ class ilUtil
 
         return $mtpl->get();
     }
-
-    /**
-    * Send Info Message to Screen.
-    *
-    * @param	string	message
-    * @param	boolean	if true message is kept in session
-    * @static
-    *
-    */
-    public static function sendInfo($a_info = "", $a_keep = false)
-    {
-        global $DIC;
-
-        $tpl = $DIC["tpl"];
-        $tpl->setOnScreenMessage("info", $a_info, $a_keep);
-    }
-
-    /**
-    * Send Failure Message to Screen.
-    *
-    * @param	string	message
-    * @param	boolean	if true message is kept in session
-    * @static
-    *
-    */
-    public static function sendFailure($a_info = "", $a_keep = false)
-    {
-        global $DIC;
-
-        if (isset($DIC["tpl"])) {
-            $tpl = $DIC["tpl"];
-            $tpl->setOnScreenMessage("failure", $a_info, $a_keep);
-        }
-    }
-
-    /**
-    * Send Question to Screen.
-    *
-    * @param	string	message
-    * @param	boolean	if true message is kept in session
-    * @static	*/
-    public static function sendQuestion($a_info = "", $a_keep = false)
-    {
-        global $DIC;
-
-        $tpl = $DIC["tpl"];
-        $tpl->setOnScreenMessage("question", $a_info, $a_keep);
-    }
-
-    /**
-    * Send Success Message to Screen.
-    *
-    * @param	string	message
-    * @param	boolean	if true message is kept in session
-    * @static
-    *
-    */
-    public static function sendSuccess($a_info = "", $a_keep = false)
-    {
-        global $DIC;
-
-        /** @var ilTemplate $tpl */
-        $tpl = $DIC["tpl"];
-        $tpl->setOnScreenMessage("success", $a_info, $a_keep);
-    }
-
+    
     
     /**
      * @deprecated use HTTP-service instead
