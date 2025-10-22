f858f977ae57f6a36f549ae9b93d9ef3ce4660c4
Thibeau Fuhrer <thibeau@sr.solutions>
2025-10-22 15:04:13 +0200

    [FIX] UI: align naming with feature (option filter) more precisely.
    * Align naming to match "option filter" more precisely
    * Apply PHP and JavaScript coding style conventions

--- components/ILIAS/UI/resources/js/Input/Field/src/input.factory.js
+++ components/ILIAS/UI/resources/js/Input/Field/src/input.factory.js
@@ -31,7 +31,7 @@ import TextareaFactory from './Textarea/textarea.factory.js';
 import MarkdownFactory from './Markdown/markdown.factory.js';
 import TreeSelectFactory from './TreeSelect/TreeSelectFactory.js';
 import JQueryEventListener from '../../../Core/src/JQueryEventListener.js';
-import InputHasOptionFilterContextFactory from './hasQuickFilterContext/hasOptionFilter.factory.js';
+import OptionFilterFactory from './OptionFilter/OptionFilterFactory.js';
 
 il.UI = il.UI || {};
 il.UI.Input = il.UI.Input || {};
@@ -39,7 +39,7 @@ il.UI.Input = il.UI.Input || {};
 (function (Input) {
   Input.textarea = new TextareaFactory();
   Input.markdown = new MarkdownFactory();
-  Input.hasOptionFilterContext = new InputHasOptionFilterContextFactory();
+  Input.optionFilter = new OptionFilterFactory();
   Input.treeSelect = new TreeSelectFactory(
     new JQueryEventListener($),
     il.UI.menu.drilldown,
